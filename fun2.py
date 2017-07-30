ffb1a3bf458203151e240f4f6ae2735ef823fca3
elias19-meet <elias19@meet.mit.edu>
2017-07-30 11:48:48 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -7,3 +7,41 @@ def sq (z):
     for i in range(z):
         print("* "*z)   
     
+def sq2 (q,w,char):
+    for i in range(q):
+        print(str(char)*w)
+        
+
+def sqs (q,w,char1,char2):
+    print(str(char1)*w)
+    for i in range(q-2):
+        
+        print(str(char1)+str(char2)*(w-2)+str(char1))
+    print(str(char1)*w)
+
+
+def fib1 (num):
+    c=1
+    b=1
+    z=b
+    for i in range(0,num):
+        c=z
+        z=z+c
+        
+    return(z)
+
+
+def fib(n):
+ a,b = 1,1
+ for i in range(n-1):
+  a,b = b,a+b
+ return a
+
+
+def fact (k):
+    z=0
+    c=0
+    for i in range (1,k+1):
+        c=c+1
+        z=z+(c+1)*c
+    return (z)
